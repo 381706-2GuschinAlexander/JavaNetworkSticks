83d119d005105db27d283452cc0d6f1db6b00f0c
Guschin Alexandr <igushin1999@gmail.com>
2020-10-29 23:35:02 +0300

    input while listening

--- src/Client.java
+++ src/Client.java
@@ -1,28 +1,51 @@
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 
-
-public class Client {
+public class Client{
     private Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
+    private boolean listener_run = true;
+
+    
 
     public void startConnection(String ip, int port) throws IOException {
         clientSocket = new Socket(ip, port);
         out = new PrintWriter(clientSocket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
+        Runnable task = () -> {
+            String resp;
+            while(listener_run){
+                try{
+                    resp = in.readLine();
+                    //System.out.println(resp);
+                    if(resp.equals("fine"))
+                        System.out.println(resp);
+                } catch (IOException e){
+                    System.out.println(e);
+                }
+            }
+            Thread.currentThread().interrupt();
+        };
 
-        String resp;
-        while(true){
-            resp = in.readLine();
-            System.out.println(resp);
-        }
+        Thread thread = new Thread(task);
+        thread.start();
+
+
+        int a;
+        String resp = in.readLine();
+        System.out.println(resp);
+        Scanner inTest = new Scanner(System.in);
+        a = inTest.nextInt();
+        inTest.close();
     }
  
 
  
     public void stopConnection() throws IOException {
+        listener_run = false;
         in.close();
         out.close();
         clientSocket.close();
