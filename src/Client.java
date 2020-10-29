1a071f0338a81add6bcd018541f52c67adad5e13
Guschin Alexandr <igushin1999@gmail.com>
2020-10-29 21:32:39 +0300

    multiServer

--- src/Client.java
+++ src/Client.java
@@ -6,18 +6,21 @@ public class Client {
     private Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
- 
+
     public void startConnection(String ip, int port) throws IOException {
         clientSocket = new Socket(ip, port);
         out = new PrintWriter(clientSocket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+
+        String resp;
+        while(true){
+            resp = in.readLine();
+            System.out.println(resp);
+        }
     }
  
-    public String sendMessage(String msg) throws IOException {
-        out.println(msg);
-        String resp = in.readLine();
-        return resp;
-    }
+
  
     public void stopConnection() throws IOException {
         in.close();
@@ -29,7 +32,6 @@ public class Client {
         System.out.println("start client");
         Client client = new Client();
         client.startConnection("127.0.0.1", 8080);
-        System.out.println(client.sendMessage("hello server"));
         System.out.println("client finished");
         client.stopConnection();
     }
--- src/Server.java
+++ src/Server.java
@@ -4,31 +4,52 @@ import java.io.*;
 
 public class Server {
     private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
+    private static boolean isGoing = true;
  
     public void start(int port) throws IOException {
         serverSocket = new ServerSocket(port);
-        clientSocket = serverSocket.accept();
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String greeting = in.readLine();
-            if ("hello server".equals(greeting)) {
-                out.println("hello client\n");
-                System.out.println("connection successful");
-            }
-            else {
-                out.println("unrecognised greeting\n");
-            }
+        while (true)
+            new EchoClientHandler(serverSocket.accept()).start();
     }
  
     public void stop() throws IOException {
-        in.close();
-        out.close();
-        clientSocket.close();
         serverSocket.close();
     }
+ 
+    private static class EchoClientHandler extends Thread  {
+        private Socket clientSocket;
+        private PrintWriter out;
+        private BufferedReader in;
+ 
+        public EchoClientHandler(Socket socket) {
+            this.clientSocket = socket;
+        }
+ 
+        public void run()  {
+            try{
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(
+              new InputStreamReader(clientSocket.getInputStream()));
+            
+            String inputLine;
+            while (isGoing) {
+                try{
+                Thread.sleep(2000);
+                } catch (InterruptedException e){
+                    System.out.println(e);
+                }
+                out.println("fine");
+            }
+
+            in.close();
+            out.close();
+            clientSocket.close();
+        } catch( IOException e){
+            System.out.println(e);
+        } 
+    }
+}
+
     public static void main(String[] args) throws IOException {
         System.out.println("start");
         Server server=new Server();
